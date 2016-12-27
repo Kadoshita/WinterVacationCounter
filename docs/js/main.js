1289177d84b8b7ff17059399b94c50f775898f6c
kadoshita <kadoshita.inct@gmail.com>
2016-12-27 13:43:26 +0900

    changed font

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,28 +5,28 @@ setInterval(function() {
 	countYear = new Date(2017, 0, 9, 8, 50);
 	Msec = countYear.getTime() - date.getTime();
 
-	countDay = Math.floor(Msec / (1000 * 60 * 60 * 24));
+	countDay=~~(Msec/(1000 * 60 * 60 * 24));
 	Msec -= (countDay * (1000 * 60 * 60 * 24));
 
-	countHour = Math.floor(Msec / (1000 * 60 * 60));
+	countHour=~~(Msec/(1000 * 60 * 60));
 	Msec -= (countHour * (1000 * 60 * 60));
 
-	countMin = Math.floor(Msec / (1000 * 60));
+	countMin=~~(Msec/(1000 * 60));
 	Msec -= (countMin * (1000 * 60));
 
-	countSec = Math.floor(Msec / 1000);
+	countSec=~~(Msec/1000);
 
 	if (countDay != 0) {
-		printData += countDay + '日';
+		printData += '<span style="font-weight: bold;">'+countDay + '</span>日';
 	}
 	if (countHour != 0) {
-		printData += countHour + '時間';
+		printData += '<span style="font-weight: bold;">'+ countHour + '</span>時間';
 	}
 	if (countMin != 0) {
-		printData += countMin + '分';
+		printData += '<span style="font-weight: bold;">'+ countMin + '</span>分';
 	}
 	if (countSec != 0) {
-		printData += countSec + '秒';
+		printData += '<span style="font-weight: bold;">'+ countSec + '</span>秒';
 	}
 	$('#count-data').html('冬休み終了まであと<br>'+printData);
 },1000);
